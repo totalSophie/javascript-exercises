6710750bd8ac8c4424ce011341a827cf64044588
Sophie <totalsophie@gmail.com>
2022-01-14 22:32:02 +0300

    Add solved solutions to the first three exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let concatanatedWord = '';
+    if (num < 0){
+        return ('ERROR')
+    }
+    
+    for (let i = 0; i < num; i++){
+        concatanatedWord += word;
+    }
+
+    return concatanatedWord;
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(str) {
+    word = '';
+    let newStr = str;
 
+    for(let i = str.length; i > 0; i--)
+    {
+        lastLetter = newStr.slice(i - 1);
+        newStr = str.slice(0, i-1);
+        word += lastLetter;        
+    }
+    
+    return word;
 };
 
 // Do not edit below this line
