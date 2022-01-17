ddf22327446b2b8db7792083db20f6672d592d27
Sophie <totalsophie@gmail.com>
2022-01-17 13:13:50 +0300

    Add the solution to the tempConversion Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = (farenheit - 32) * 5/9;
+  if (celcius % 1 === 0){
+    return celcius;
+  }
+  else{
+    return parseFloat(celcius.toFixed(1));
+  }
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let farenheit = celcius * 9/5 + 32;
 
+  if (farenheit % 1 === 0){
+    return farenheit;
+  }
+  else{
+    return parseFloat(farenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
