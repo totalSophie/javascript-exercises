6ee479536fc2691916f5dcdbf2ff1eb4fb2a29e2
Sophie <totalsophie@gmail.com>
2022-01-31 17:05:59 +0300

    Add the result of the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(a, b) {
+  return (parseInt(a) + parseInt(b));
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return (parseInt(a) - parseInt(b));
+};
+
+const sum = function(arr) {
+	const arraySum = arr.reduce(function sumArray(total, number){return total + number}, 0)
+  return arraySum
+};
+
+const multiply = function(arr) {
+  const arrayMultiply = arr.reduce(function multipyArray(a, b){return a*b})
+  return arrayMultiply
+};
+
+const power = function(a, b) {
+  return (a**b)
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num) {
+	//5 4 3 2 1
+  let answer = 1;
+  if(num === 0){
+    return answer
+  }
+  else{
+    for (let i = num; i > 0; i--){
+      answer = answer * i
+    }
+    return answer;
+  }
+  
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
