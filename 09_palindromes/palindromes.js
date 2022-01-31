69d3d6ab284f1cf3d13fbeec96297e6e0d29daec
Sophie <totalsophie@gmail.com>
2022-01-31 21:27:53 +0300

    Add the palindrom exercise solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let res = str.replace(/[ ,!?".]/g, "");
+    res = res.toLowerCase();
+    let newWord = '';
+    for (let i = (res.length) - 1; i >= 0; i--){
+        newWord += res[i]
+    }
+    if(newWord == res){
+        return true
+    }
+    else{return false}
 
 };
 
