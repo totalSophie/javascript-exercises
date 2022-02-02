712a6db9e49e32840c02338d9e1f4b3dc2eef0a6
Sophie <totalsophie@gmail.com>
2022-02-02 14:53:31 +0300

    Adds the solution to the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arrayNumbers = [0, 1,1]
+    num = parseInt(num);
+    if (num < 0){
+        return 'OOPS';
+    }
 
-};
+    else if (num == 0 || num == 1){
+        let fibNumber = 1;
+        return fibNumber;
+    }
+
+    else{
+        for (let i = 2; i < (num); i++)
+        {
+            newFibNumber = arrayNumbers[i] + arrayNumbers[(i-1)]
+            arrayNumbers.push(newFibNumber)
+        }
+        
+        return arrayNumbers[num];
+    }
+    
+
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
