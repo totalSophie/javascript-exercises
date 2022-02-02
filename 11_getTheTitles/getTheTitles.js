eee4a2aa7d8d64ea7f7f7b266fc0f4ea98039e28
Sophie <totalsophie@gmail.com>
2022-02-02 15:13:23 +0300

    Add the solution to the getThe Tiles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titleArray = array.map(book => book.title )
+    return titleArray;
 };
 
 // Do not edit below this line
