7665ff3613dfcfbfa1c4653af9588621b00be717
Sophie <totalsophie@gmail.com>
2022-02-04 12:48:17 +0300

    Add the solution to findTheOldest JS exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+//const { TestScheduler } = require("jest");
+const findTheOldest = function(arr) {
+    let name
+    let max = 0
 
+    for (let i = 0; i < arr.length; i++){
+        if (!arr[i].yearOfDeath){
+            let age = new Date().getFullYear() - arr[i].yearOfBirth
+            if (age > max){
+                max = age
+                name = arr[i]
+            }
+        }
+
+        else{
+            let age = arr[i].yearOfDeath - arr[i].yearOfBirth 
+            if (age > max){
+                max = age
+                name = arr[i]
+            }
+            }       
+
+    }
+
+    return name;
+    
+    
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
